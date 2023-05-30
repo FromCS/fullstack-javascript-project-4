8b7741f374879caadb51bd9248d6367263b1e7ef
Mikhail Gostjukhin <mkg0st@yandex.ru>
2023-05-30 17:30:01 +0300

    Refactor loading HTML file.
    Add loading local images and changing source of it.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import axios from 'axios';
 import * as fs from 'fs/promises';
 import path from 'path';
 import getValidFilename from './getValidFilename.js';
+import loadImages from './loadImages.js';
 
 const pageLoader = (url, output = process.cwd()) => {
-  const [, hostname] = url.split('//');
-  const filename = getValidFilename(hostname);
-  return axios.get(url)
-    .then(({ data }) => fs.writeFile(path.join(output, filename), data))
-    .catch(console.error);
+  const loadingURL = new URL(url);
+  const loadingFilename = getValidFilename(loadingURL);
+  const loadingFilepath = path.join(output, loadingFilename);
+  const html = axios.get(loadingURL)
+    .then(({ data }) => {
+      fs.writeFile(loadingFilepath, data);
+      return data;
+    });
+
+  const imagePromises = html
+    .then((data) => loadImages(loadingURL, data, output, loadingFilepath));
+  return imagePromises;
 };
 
 export default pageLoader;
