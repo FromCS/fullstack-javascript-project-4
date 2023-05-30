d70e66079cc112b386a4b63e63e935dca117b79d
Mikhail Gostjukhin <mkg0st@yandex.ru>
2023-05-30 17:28:45 +0300

    Add new type of files: file, dir & image.
    Realize validation for each.

--- src/getValidFilename.js
+++ src/getValidFilename.js
@@ -1,1 +1,16 @@
-export default (hostname) => `${hostname.replace(/[^a-zA-Z0-9]/gm, '-')}.html`;
+export default (url, type = 'file') => {
+  const hostAndPathName = `${url.hostname}${url.pathname}`;
+  switch (type) {
+    case 'dir': {
+      return `${hostAndPathName.replace(/[^a-zA-Z0-9]/gm, '-')}_files`;
+    }
+    case 'image': {
+      const extension = url.toString().endsWith('.png') ? '.png' : 'jpg';
+      const [name] = url.toString().split(extension);
+      return `${name.replace(/[^a-zA-Z0-9]/gm, '-')}${extension}`;
+    }
+    default: {
+      return `${hostAndPathName.replace(/[^a-zA-Z0-9]/gm, '-')}.html`;
+    }
+  }
+};
